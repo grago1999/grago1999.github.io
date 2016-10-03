24a076b7bebc2234acb2ed2126e71941892a5e22
grago1999 <grago1999@gmail.com>
2016-10-02 21:45:21 -0400

    1.9.1
    - Removed hiding on load, moved functionality from js to css

--- js/main.js
+++ js/main.js
@@ -4,10 +4,7 @@ window.onfocus = function () { document.title = "Who's Gianluca?"; }
 var ids = [];
 var currentId = "";
 
-$(document).ready(function() {
-	console.log("You found me ( ͡° ͜ʖ ͡°). Look around for some other easter eggs!")
-	hideAll(true);
-});
+console.log("You found me ( ͡° ͜ʖ ͡°). Look around for some other easter eggs!")
 
 function setup(pageId) {
 	if (pageId == 1) {
@@ -19,13 +16,9 @@ function setup(pageId) {
 	}
 }
 
-function hideAll(isInitial) {
+function hideAll() {
 	for (var i = 0; i < ids.length; i++) {
-		if (isInitial) {
-			$(ids[i]+"Info").hide();
-		} else {
-			$(ids[i]+"Info").slideUp(750);
-		}
+		$(ids[i]+"Info").slideUp(750);
 	}
 }
 
@@ -34,7 +27,7 @@ function selectedItem(id) {
 		currentId = "";
 		$("#"+id+"Info").slideUp(750);
 	} else {
-		hideAll(false);
+		hideAll();
 		currentId = id;
 		$("#"+id+"Info").slideDown(750);
 	}
