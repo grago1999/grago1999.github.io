aa5e3737196c7f5351b2fde5714b3ceede4fe961
grago1999 <grago1999@gmail.com>
2016-10-17 23:30:53 -0400

    2.0.0
    - Content and CSS updates

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ window.onfocus = function () { document.title = "Who's Gianluca?"; }
 
 var ids = [];
 var currentId = "";
+var duration = 750;
 
-console.log("You found me ( ͡° ͜ʖ ͡°). Look around for some other easter eggs!")
+console.log("You found me ( ͡° ͜ʖ ͡°). Look around for some other surprises!")
 
 function setup(pageId) {
 	if (pageId == 1) {
-		ids = ["#education", "#company", "#blog", "#design"];
+		ids = ["#education", "#company", "#blog", "#languages", "#design"];
 	} else if (pageId == 2) {
-		ids = ["#conjungo", "#vrkit", "#graphingkit", "#followingtech", "#mdkit"];
+		ids = ["#conjungo", "#vrkit", "#discemus", "#lookingglass", "#followingtech", "#mdkit", "#helihavoc", "#graphingkit", "#gemini"];
 	} else {
 		ids = ["#email", "#github", "#linkedin", "#wwdcscholars"];
 	}
@@ -18,17 +19,20 @@ function setup(pageId) {
 
 function hideAll() {
 	for (var i = 0; i < ids.length; i++) {
-		$(ids[i]+"Info").slideUp(750);
+		$(ids[i]+"Info").slideUp(duration);
 	}
 }
 
 function selectedItem(id) {
 	if (id == currentId) {
 		currentId = "";
-		$("#"+id+"Info").slideUp(750);
+		$("#"+id+"Info").slideUp(duration);
 	} else {
 		hideAll();
 		currentId = id;
-		$("#"+id+"Info").slideDown(750);
+		$("#"+id+"Info").slideDown(duration);
 	}
+	$('html, body').animate({
+        scrollTop: $("#"+id).offset().top-window.innerHeight/4
+    }, duration);
 }
